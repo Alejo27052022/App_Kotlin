6c9665f6f61049f92964ce2e9bae2a3c3068d7cc
Stalin <stalinalejandro19@outlook.es>
2023-06-15 15:41:58 -0500

    Avance

--- app/src/main/java/com/example/app/Code/Calculo.kt
+++ app/src/main/java/com/example/app/Code/Calculo.kt
@@ -40,18 +40,258 @@ fun CalculoCanal(){
                 println("Cuanto pesa su bovino macho")
                 var peso = readLine()!!.toFloat();
                 resultado = ((65.00/100) * peso).toFloat();
-
+                resultado_libra = (resultado * 2.20462).toFloat();
 
                 println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
             }
             else if (sexo_bovino == "hembra"){
                 println("Cuanto pesa su bovino hembra")
                 var peso = readLine()!!.toFloat();
                 resultado = ((58.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
 
                 println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
             }
         }
+
+        razas[2] -> {
+            println("Ha elegido la raza " + razas[2])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((63.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((58.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[3] -> {
+            println("Ha elegido la raza " + razas[3])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((60.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((58.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[4] -> {
+            println("Ha elegido la raza " + razas[4])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((58.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((55.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[5] -> {
+            println("Ha elegido la raza " + razas[5])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((54.60/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((50.80/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[6] -> {
+            println("Ha elegido la raza " + razas[6])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((51.29/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((51.29/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[7] -> {
+            println("Ha elegido la raza " + razas[7])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((56.41/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((56.07/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[8] -> {
+            println("Ha elegido la raza " + razas[8])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((56.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((55.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[9] -> {
+            println("Ha elegido la raza " + razas[9])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((63.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((55.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
+        razas[10] -> {
+            println("Ha elegido la raza " + razas[10])
+            println("Su bovino es macho o hembra ")
+            var sexo_bovino = readLine().toString();
+            if(sexo_bovino == "macho"){
+                println("Cuanto pesa su bovino macho")
+                var peso = readLine()!!.toFloat();
+                resultado = ((62.45/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de macho en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de macho en libra pesa " + resultado_libra);
+
+            }
+            else if (sexo_bovino == "hembra"){
+                println("Cuanto pesa su bovino hembra")
+                var peso = readLine()!!.toFloat();
+                resultado = ((58.00/100) * peso).toFloat();
+                resultado_libra = (resultado * 2.20462).toFloat();
+
+                println("El Rendimiento de canal del peso de hembra en Kg pesa " + resultado);
+                println("El Rendimiento de canal del peso de hembra en libra pesa " + resultado_libra);
+
+            }
+        }
+
     }
     /*
     println(razas[0])
