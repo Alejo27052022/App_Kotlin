1efb0c1c06b709b3e97077bef66be8d27a4b22e7
Stalin <stalinalejandro19@outlook.es>
2023-06-15 15:24:31 -0500

    Avance

--- app/src/main/java/com/example/app/Pages/RendimientoCalculo.kt
+++ app/src/main/java/com/example/app/Pages/RendimientoCalculo.kt
@@ -2,11 +2,27 @@ package com.example.app.Pages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.app.R
+import com.example.app.databinding.ActivityRendimientoCalculoBinding
 
 class RendimientoCalculo : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRendimientoCalculoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rendimiento_calculo)
+        binding = ActivityRendimientoCalculoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val items = listOf("Aberdeen Angus", "Brahmán", "Charolais", "Hereford", "Limousin", "Nelore", "Brow Swiss", "Holstein", "Normado", "Jersey", "Gando de Lidia");
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+        binding.dropdownMenuFieldRaza.setAdapter(adapter)
+
+        val sexo = listOf("Macho", "Hembra")
+        val adapter_sexo = ArrayAdapter(this, R.layout.list_item, sexo)
+        binding.dropdownMenuFieldSexo.setAdapter(adapter_sexo)
+
+
+
     }
 }
