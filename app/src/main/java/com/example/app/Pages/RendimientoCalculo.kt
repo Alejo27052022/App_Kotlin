a127784b0b23689c148fb00fe3e2d825758943bd
ESTUDIANTES <alejandro123@gmail.com>
2023-06-15 19:01:33 -0500

    Avance Project

--- app/src/main/java/com/example/app/Pages/RendimientoCalculo.kt
+++ app/src/main/java/com/example/app/Pages/RendimientoCalculo.kt
@@ -1,8 +1,11 @@
 package com.example.app.Pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageButton
 import com.example.app.R
 import com.example.app.databinding.ActivityRendimientoCalculoBinding
 
@@ -21,6 +24,10 @@ class RendimientoCalculo : AppCompatActivity() {
         val sexo = listOf("Macho", "Hembra")
         val adapter_sexo = ArrayAdapter(this, R.layout.list_item, sexo)
         binding.dropdownMenuFieldSexo.setAdapter(adapter_sexo)
+
+        val unidad = listOf("Lb", "Kg")
+        val adapter_unidad = ArrayAdapter(this, R.layout.list_item, unidad)
+        binding.dropdownMenuFieldUnidad.setAdapter(adapter_unidad)
 
 
 
