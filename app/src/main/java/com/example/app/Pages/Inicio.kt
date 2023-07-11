d338416f5b7a375421db79ca386c1bd1edd2eb25
ESTUDIANTES <alejandro123@gmail.com>
2023-07-11 16:51:54 -0500

    add

--- app/src/main/java/com/example/app/Pages/Inicio.kt
+++ app/src/main/java/com/example/app/Pages/Inicio.kt
@@ -3,26 +3,48 @@ package com.example.app.Pages
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageButton
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.app.R
+import com.google.android.material.navigation.NavigationView
+
 
 class Inicio : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
-        val imgbtnManejo: ImageButton = findViewById(R.id.imgbtnManejo)
+        setToolBar()
         val home: ImageButton = findViewById(R.id.home_btn)
-
-        imgbtnManejo.setOnClickListener{
-            val intent: Intent = Intent(this, Page2::class.java)
-            startActivity(intent)
-        }
+        var drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView : NavigationView = findViewById(R.id.navview)
 
         home.setOnClickListener{
             val intent: Intent = Intent(this, Inicio::class.java)
             startActivity(intent)
         }
-
     }
+    private fun setToolBar(){
+        val toolbar : Toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_home)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
 }
