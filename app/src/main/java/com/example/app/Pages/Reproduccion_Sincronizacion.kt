3119bfa642beb03ee0a33beffa961279ef0cbcc5
Stalin <stalinalejandro19@outlook.es>
2023-08-08 15:33:17 -0500

    Add Section Reproduccion

---
+++ app/src/main/java/com/example/app/Pages/Reproduccion_Sincronizacion.kt
@@ -0,0 +1,15 @@
+package com.example.app.Pages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.R
+
+class Reproduccion_Sincronizacion : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reproduccion_sincronizacion)
+
+
+
+    }
+}
